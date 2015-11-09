6f042fe462f27285dff3a00b5be4c1adb22eac7f
marone507 <marone5075@gmail.com>
2015-11-09 16:09:48 +0100

    login Page added

--- scripts/sernmot.js
+++ scripts/sernmot.js
@@ -8,34 +8,13 @@ function setValue(evt) {
     //evt.target.parentElement.parentElement.getElementsByClassName("value")[0].innerHTML = evt.target.value;
 }
 
+function connect(evt) {
+    alert("Connecting...");   
+}
+
 function init()
 {
-    var buttons = document.getElementsByClassName("listElement");
-    buttons[0].addEventListener("click", function(){
-                                    document.getElementById("loginPage").style.visibility = 'hidden';
-                                    document.getElementById("sensorsPage").style.visibility = 'hidden';
-                                    document.getElementById("motorsPage").style.display = 'block';
-    });
-    buttons[1].addEventListener("click", function(){
-                                    document.getElementById("loginPage").style.visibility = 'hidden';
-                                    document.getElementById("motorsPage").style.display = 'none';
-                                    document.getElementById("sensorsPage").style.visibility = 'visible';
-    }); 
     
-    var servos = document.getElementsByClassName("servosContainer");
-    for (i = 0; i < servos.length; i++) {
-        servos[i].addEventListener("input", setSliderText, false);
-    }
-    
-    var motors = document.getElementsByClassName("motorsContainer");
-    for (i = 0; i < motors.length; i++) {
-        motors[i].addEventListener("input", setSliderText, false);
-    }
-    
-    var updateButtons = document.getElementsByClassName("updateButton");
-    for (i = 0; i < updateButtons.length; i++) {
-        updateButtons[i].addEventListener("click", setValue, false);
-    }
 }
 
 init();
@@ -63,5 +42,39 @@ $("#editButton").click(function(){
 
 $(document).ready(function(){
     $("#dropDown").hide();
+    
+    var buttons = document.getElementsByClassName("listElement");
+    buttons[0].addEventListener("click", function(){
+                                    document.getElementById("loginPage").style.visibility = 'visible';
+                                    document.getElementById("sensorsPage").style.visibility = 'hidden';
+                                    document.getElementById("motorsPage").style.display = 'none';
+    });
+    buttons[1].addEventListener("click", function(){
+                                    document.getElementById("loginPage").style.visibility = 'hidden';
+                                    document.getElementById("sensorsPage").style.visibility = 'hidden';
+                                    document.getElementById("motorsPage").style.display = 'block';
+    });
+    buttons[2].addEventListener("click", function(){
+                                    document.getElementById("loginPage").style.visibility = 'hidden';
+                                    document.getElementById("motorsPage").style.display = 'none';
+                                    document.getElementById("sensorsPage").style.visibility = 'visible';
+    }); 
+    
+    var servos = document.getElementsByClassName("servosContainer");
+    for (i = 0; i < servos.length; i++) {
+        servos[i].addEventListener("input", setSliderText, false);
+    }
+    
+    var motors = document.getElementsByClassName("motorsContainer");
+    for (i = 0; i < motors.length; i++) {
+        motors[i].addEventListener("input", setSliderText, false);
+    }
+    
+    var updateButtons = document.getElementsByClassName("updateButton");
+    for (i = 0; i < updateButtons.length; i++) {
+        updateButtons[i].addEventListener("click", setValue, false);
+    }
+    
+    document.getElementById("connectButton").addEventListener("click", connect, false);
 });
 
