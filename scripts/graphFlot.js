63bb8904c7ea55fe38239049150d29108b0de38e
Daniel Honies <daniel.honies@gmail.com>
2016-02-07 15:57:08 +0100

    graphs are now getting displayed with random values

--- scripts/graphFlot.js
+++ scripts/graphFlot.js
@@ -11,22 +11,56 @@ function MyFlotChart(dest, maxGraphPoints, graphName, minBound, maxBound)
     // We use an inline data source in the example, usually data would
     // be fetched from a server
     this.name = graphName;
-    this.drawing = "0";
+    this.drawing = "1";
     this.maxPoints = maxGraphPoints;
-    this.data = [];
+    console.log(maxGraphPoints);
+    this.data = [],
+            totalPoints = 300;
     this.xValue = 0;
 
     var that = this;
 
-    // Set up the control widget
+
+    this.getRandomData =function () {
+            
+            if (that.data.length > 0)
+                that.data = that.data.slice(1);
+
+            // Do a random walk
+
+            while (that.data.length < that.maxPoints) {
+
+                var prev = that.data.length > 0 ? that.data[that.data.length - 1] : 50,
+                    y = getRandomIntIncl(minBound, maxBound);
+
+                if (y < 0) {
+                    y = 0;
+                } else if (y > 1000) {
+                    y = 1000;
+                }
+
+                that.data.push(y);
+            }
+
+            // Zip the generated y values with the x values
+
+            var res = [];
+            for (var i = 0; i < that.data.length; ++i) {
+                res.push([i, that.data[i]])
+            }
+
+            return res;
+        }
+    
     for	(var i = 0; i < that.maxPoints; i++) {
-        that.data.push([i, 0]);
+        //that.data.push([i, 500]);
     }
-    //console.log(that.data);
+    console.log(that.data);
+    console.log(that.data.length);
     this.updateInterval = 100;
     var rand = [that.xValue, getRandomIntIncl(minBound, maxBound)];
-    that.data[that.xValue] = rand;
-    this.plot = $.plot(dest, that.data,  {
+    //that.data[that.xValue] = rand;
+    this.plot = $.plot(dest, [ that.getRandomData() ],  {
         series: {
             shadowSize: 0,	// Drawing is faster without shadows
         },
@@ -35,7 +69,7 @@ function MyFlotChart(dest, maxGraphPoints, graphName, minBound, maxBound)
             max: maxBound,
         },
         xaxis: {
-            show: false,
+            show: true,
         },
         grid : {
             borderWidth: {
@@ -49,8 +83,8 @@ function MyFlotChart(dest, maxGraphPoints, graphName, minBound, maxBound)
     });
     that.xValue++;
     this.resize = function() {
-        that.plot.resize();  
-        that.plot.setupGrid();
+        //that.plot.resize();  
+        //that.plot.setupGrid();
         that.plot.draw();
         console.log("resizing graph of " + that.name + " worked");
     }
@@ -59,14 +93,14 @@ function MyFlotChart(dest, maxGraphPoints, graphName, minBound, maxBound)
         if (that.drawing == "1")
         {
             rand = [that.xValue, getRandomIntIncl(minBound, maxBound)];
-            that.data[that.xValue] = rand;
+            //that.data[that.xValue] = rand;
             that.xValue++;
-            that.plot.setData(that.data);
+            that.plot.setData([ that.getRandomData() ]);
             
-            if (that.name == 'analog 0')
-            {
+            
+            
                 console.log(that.name + ' : ' + that.data[that.xValue-1] + ' : ' + (that.xValue-1));
-            }
+            
             // Since the axes don't change, we don't need to call plot.setupGrid()
 
             that.plot.draw();
